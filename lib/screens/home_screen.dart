8abf7dd4af88892228f75c50409073878db181ba
Suraj Yadav <surajyadav200701@gmail.com>
2024-03-16 15:38:57 +0530

    completed it , but it only works for sdk 28

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,28 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+   var text = "Click on Mic to Record !";
+  SpeechToText speechToText = SpeechToText();
+
+  var isListening = false;
+ 
+
+  void checkMic() async {
+    bool micAvailable = await speechToText.initialize();
+
+    if (micAvailable) {
+      print("MicroPhone Available");
+    } else {
+      print("User denied the use of speech microphone");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkMic();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,16 +44,45 @@ class _HomeScreenState extends State<HomeScreen> {
             // mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-             const SizedBox(height: 120,),
-              const Text("Click on Mic to Record !"),
-           const SizedBox(height: 20,),
+              const SizedBox(
+                height: 120,
+              ),
+              Text(text),
+              const SizedBox(
+                height: 20,
+              ),
               InkWell(
-                onTap: () {},
-                child: const CircleAvatar(
-                  child: Icon(Icons.mic),
+                onTap: () async {
+                  if (!isListening) {
+                    bool micAvailable = await speechToText.initialize();
+                    if (micAvailable) {
+                      setState(() {
+                        isListening = true;
+                      });
+
+                      speechToText.listen(
+                          listenFor: const Duration(seconds: 30),
+                          onResult: (result) {
+                            setState(() {
+                              text = result.recognizedWords;
+                              isListening = false;
+                            });
+                          });
+                    } else {
+                      setState(() {
+                        isListening = false;
+
+                        speechToText.stop();
+                      });
+                    }
+                  }
+                },
+                child: CircleAvatar(
+                  child: isListening
+                      ? const Icon(Icons.record_voice_over)
+                      : const Icon(Icons.mic),
                 ),
               ),
-             
             ],
           ),
         ),
